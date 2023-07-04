24ca4fe6a4e3de732917c39bf5960c87b98dc6c3
SaidulIslam16 <isaidulislamsayed@gmail.com>
2023-07-04 21:18:16 +0600

    toast added with correct and wrong answer

--- src/components/QuizBody/QuizBody.js
+++ src/components/QuizBody/QuizBody.js
@@ -1,5 +1,7 @@
-import React, { createContext, useContext } from 'react';
+import React, { createContext } from 'react';
 import Options from '../Options/Options';
+import { toast, ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 export const AnswerContext = createContext();
 
 const QuizBody = ({ quiz }) => {
@@ -11,24 +13,33 @@ const QuizBody = ({ quiz }) => {
         console.log(answer.target.value);
         // answer = answer.target.value
         if (answer.target.value === correctAnswer) {
-            alert('Correct Answer');
+            toast.success(`Correct Answer: ${answer.target.value}`)
         }
         else {
-            alert('wrong answer')
+            toast.warning('Wrong Answer')
         }
+    }
+
+    // Show Correct answer function
+
+    const showCorrectAnswer = () => {
+        toast.info(`Correct answer: ${correctAnswer}`, {
+            closeOnClick: true,
+        });
     }
     return (
         <AnswerContext.Provider value={[checkAnswer]}>
             <div className=' p-4 shadow-md rounded-lg'>
-                <div className=''>
+                <div className='flex items-center justify-around'>
                     <h3 className='text-xl font-bold text-center'>{question}</h3>
-                    <button>Check Correct</button>
+                    <button onClick={showCorrectAnswer}>Check Correct</button>
                 </div>
                 <div className='grid grid-cols-2 gap-3 mt-5'>
                     {
                         options.map((option, key) => <Options option={option} key={key} question={question}></Options>)
                     }
                 </div>
+                <ToastContainer></ToastContainer>
             </div>
         </AnswerContext.Provider>
     );
